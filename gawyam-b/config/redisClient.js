abd555cf7140e76a293e7cac2d79025dd8f56360
Nira00A <biswasarin341@gmail.com>
2026-01-11 18:51:32 +0530

    Redis connection string from Upstash is added

--- gawyam-b/config/redisClient.js
+++ gawyam-b/config/redisClient.js
@@ -1,6 +1,10 @@
 const {Redis} = require('ioredis')
 
-const redis = new Redis({})
+const redis = new Redis(process.env.UPSTASH_REDIS_URL , {
+    tls: {
+        rejectUnauthorized: false 
+    }
+})
 
 redis.on('connect', () => console.log('🚀 Redis Connected!'));
 redis.on('error', (err) => console.log('❌ Redis Error:', err));
