9abca645be1b1127045a3ef293997f6add563b2e
Nira00A <biswasarin341@gmail.com>
2026-01-11 18:08:03 +0530

    PostgreSql's connection string established using supabase as a hosting db

--- gawyam-b/config/postgreSqlClient.js
+++ gawyam-b/config/postgreSqlClient.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
+    /*
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    */
+
+    connectionString: process.env.SUPABASE_DATABASE_URL,
+    ssl: { rejectUnauthorized: false}
 });
 
 module.exports = pool
